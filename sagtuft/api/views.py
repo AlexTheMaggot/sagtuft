3e44682faf36e926bbc9c90f2cc97c9bba0ff43b
alexthemaggot <alexthemaggot23@gmail.com>
2024-06-06 14:41:20 +0500

    - create api view

--- sagtuft/api/views.py
+++ sagtuft/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseForbidden
+from .api_methods import *
 
-# Create your views here.
+
+methods = {
+    'CategoryGetList': category_list,
+    'CategoryGetDetail': category_detail,
+}
+
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        request_data = json.loads(request.body)
+        method = request_data['method']
+        if method in methods.keys():
+            run = methods[request_data['method']]
+            return run(request, request_data)
+        else:
+            return make_error(request_data['id'], 1002)
+    else:
+        return HttpResponseForbidden()
