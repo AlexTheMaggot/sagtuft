973c902cee9ed92b7d1752e3b2816c2adc410d6f
alexthemaggot <alexthemaggot23@gmail.com>
2024-06-06 14:49:17 +0500

    - add dynamic categories to english component

--- sagtuft/mainapp/src/components/Catalog/CatalogEn.js
+++ sagtuft/mainapp/src/components/Catalog/CatalogEn.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {useParams} from "react-router-dom";
 import './Catalog.css'
+import api_sender from "../api_sender";
 
 export default function CatalogEn(props) {
     let params = useParams()
@@ -10,6 +11,17 @@ export default function CatalogEn(props) {
 class CatalogClass extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            categories: [],
+        }
+    }
+
+
+    componentDidMount() {
+        let categories = api_sender('CategoryGetList', 1)
+        categories.then((response) => {
+            this.setState({categories: response.result})
+        })
     }
 
     render() {
@@ -28,46 +40,17 @@ class CatalogClass extends Component {
                         <p className="breadcrumbs__text">Catalog</p>
                     </div>
                     <div className="post__wrapper">
-                        <a href="tufting_covers.html" className="post">
-                            <img alt="" src="../../static/img/c01.webp" className="post__img"/>
-                            <div className="post__content">
-                                <div>
-                                    <p className="post__title">Tufted coverings</p>
-                                    <p>The optimal ratio of cost and quality of tufted carpet is used
-                                         consistently high demand due to a number of advantages</p>
+                        {this.state.categories.map(c => (
+                            <a key={c.id} href="tufting_covers.html" className="post">
+                                <img alt="" src={c.img} className="post__img"/>
+                                <div className="post__content">
+                                    <div>
+                                        <p className="post__title">{c.name_en}</p>
+                                        <p>{c.description_en}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
-                        <a href="artificial_turf.html" className="post">
-                            <img alt="" src="../../static/img/c02.webp" className="post__img"/>
-                            <div className="post__content">
-                                <div>
-                                    <p className="post__title">Artificial Lawn</p>
-                                    <p>The optimal ratio of cost and quality of tufted carpet is used
-                                         consistently high demand due to a number of advantages</p>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="tufting_covers.html" className="post">
-                            <img alt="" src="../../static/img/c03.webp" className="post__img"/>
-                            <div className="post__content">
-                                <div>
-                                    <p className="post__title">Velveteen</p>
-                                    <p>The optimal ratio of cost and quality of tufted carpet is used
-                                         consistently high demand due to a number of advantages</p>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="tufting_covers.html" className="post">
-                            <img alt="" src="../../static/img/c04.webp" className="post__img"/>
-                            <div className="post__content">
-                                <div>
-                                    <p className="post__title">Thread</p>
-                                    <p>The optimal ratio of cost and quality of tufted carpet is used
-                                         consistently high demand due to a number of advantages</p>
-                                </div>
-                            </div>
-                        </a>
+                            </a>
+                        ))}
                     </div>
                 </section>
             </main>
