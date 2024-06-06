10d9d2f8af2275f2e81085b5b99f18e8e1152ad1
alexthemaggot <alexthemaggot23@gmail.com>
2024-06-06 14:40:57 +0500

    - create category model

--- sagtuft/api/models.py
+++ sagtuft/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name_ru = models.CharField(max_length=100, verbose_name='Название на русском')
+    name_en = models.CharField(max_length=100, verbose_name='Название на английском')
+    name_uz = models.CharField(max_length=100, verbose_name='Название на узбекском')
+    img = models.ImageField(upload_to='categories/', verbose_name='Изображение (200x200)')
+    description_ru = models.TextField(verbose_name='Описание на русском')
+    description_en = models.TextField(verbose_name='Описание на английском')
+    description_uz = models.TextField(verbose_name='Описание на узбекском')
+
+    def __str__(self):
+        return self.name_ru
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
