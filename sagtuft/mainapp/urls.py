0be46acd2085260d432c9d42c9e6b36033d2d86e
alexthemaggot <alexthemaggot23@gmail.com>
2024-06-12 14:55:04 +0500

    - add category detail url

--- sagtuft/mainapp/urls.py
+++ sagtuft/mainapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('catalog/', views.index, name='catalog'),
+    path('catalog/<slug:category_slug>/', views.index, name='category_detail'),
     path('about/', views.index, name='about'),
     path('vacancies/', views.index, name='vacancies'),
     path('contacts/', views.index, name='contacts'),
