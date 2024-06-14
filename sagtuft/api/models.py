c56dac54ca6c7619adece46a598a6f465e20a842
alexthemaggot <alexthemaggot23@gmail.com>
2024-06-14 23:01:07 +0500

    - add collection and product models, and add having collections field in category model

--- sagtuft/api/models.py
+++ sagtuft/api/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
     description_en = models.TextField(verbose_name='Описание на английском')
     description_uz = models.TextField(verbose_name='Описание на узбекском')
     slug = models.SlugField(verbose_name='URL', unique=True, db_index=True, max_length=100)
+    has_collections = models.BooleanField(verbose_name='Есть коллекции')
 
     def __str__(self):
         return self.name_ru
@@ -17,3 +18,34 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Collection(models.Model):
+    name_ru = models.CharField(max_length=100, verbose_name='Название на русском')
+    name_en = models.CharField(max_length=100, verbose_name='Название на английском')
+    name_uz = models.CharField(max_length=100, verbose_name='Название на узбекском')
+    slug = models.SlugField(verbose_name='URL', unique=True, db_index=True, max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return self.name_ru
+
+    class Meta:
+        verbose_name = 'Коллекция'
+        verbose_name_plural = 'Коллекции'
+
+
+class Product(models.Model):
+    name_ru = models.CharField(max_length=100, verbose_name='Название на русском')
+    name_en = models.CharField(max_length=100, verbose_name='Название на английском')
+    name_uz = models.CharField(max_length=100, verbose_name='Название на узбекском')
+    slug = models.SlugField(verbose_name='URL', unique=True, db_index=True, max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', null=True, blank=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, verbose_name='Коллекция', null=True, blank=True)
+
+    def __str__(self):
+        return self.name_ru
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
